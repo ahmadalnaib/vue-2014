51b80eb798c2b4ab67a58e98e35d0a99034eb6a2
ahmedhabeeb73 <alnaib888@gmail.com>
2024-07-09 20:16:51 +0200

    add router

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,9 @@ import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
 import HomeCards from './components/HomeCards.vue';
 import JobListingsVue from './components/JobListings.vue';
-
+import { RouterView } from 'vue-router';
 </script>
 <template>
   <Navbar />
-  <Hero />
-  <HomeCards />
-  <JobListingsVue :limit="3" :showButton="true"/>
+  <RouterView />
 </template>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListingsVue from '@/components/JobListings.vue';
+</script>
+
+<template>
+  <Hero />
+  <HomeCards />
+  <JobListingsVue :limit="3" :showButton="true" />
+</template>
