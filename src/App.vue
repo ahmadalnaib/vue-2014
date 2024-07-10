3f85b8143a8501ef4205379888abb823f10bcc58
ahmad.al-naib <alnaib888@gmail.com>
2024-07-10 09:42:00 +0200

    remove it

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListingsVue from './components/JobListings.vue';
+
 import { RouterView } from 'vue-router';
 </script>
 <template>
