62bbce0bdcc234a39b73a977a19786869b89eec5
ahmedhabeeb73 <alnaib888@gmail.com>
2024-07-10 22:11:22 +0200

    add hover

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink,useRoute } from 'vue-router';
 import logo from '@/assets/img/logo.png'
+
+const isActivelink=(routePath)=>{
+  const route=useRoute();
+  return route.path ===routePath;
+}
 </script>
 
 
@@ -22,15 +27,21 @@ import logo from '@/assets/img/logo.png'
               <div class="flex space-x-2">
                 <RouterLink
                   to="/"
-                  class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  :class="[isActivelink('/') ? 
+                  'bg-green-900 ' : 
+                  'hover:bg-gray-100 hover:text-white','text-white px-3 py-2']"
                   >Home</RouterLink>
                 <RouterLink
                   to="/jobs"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                  :class="[isActivelink('/jobs') ? 
+                  'bg-green-900 ' : 
+                  'hover:bg-gray-100 hover:text-white','text-white px-3 py-2']"
                   >Jobs</RouterLink>
                 <RouterLink
                   to="/jobs/add"  
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                  :class="[isActivelink('/jobs/add') ? 
+                  'bg-green-900 ' : 
+                  'hover:bg-gray-100 hover:text-white','text-white px-3 py-2']"
                   >Add Job</RouterLink
                 >
               </div>
