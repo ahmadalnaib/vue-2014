fc59ad9ea74b8c8098c516c3483c1879c7f53573
ahmedhabeeb73 <alnaib888@gmail.com>
2024-07-10 20:32:52 +0200

    add more route

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,7 @@
 <script setup>
 import jobData from '@/j.json';
 import { ref,defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
 import JobListing from '@/components/JobListing.vue';
 
 defineProps({
@@ -33,10 +34,10 @@ const jobs = ref(jobData);
   </section>
 
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
 </template>
