fd453211cac5e3f7373cb00fd667a3b1761caf8d
ahmedhabeeb73 <alnaib888@gmail.com>
2024-07-14 17:35:01 +0200

    config the api link

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BackButton from '@/components/BackButton.vue';
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
 
@@ -16,7 +17,7 @@ const state = reactive({
 
 onMounted(async () => {
   try {
-    const response = await axios.get(`http://localhost:3000/jobs/${jobId}`);
+    const response = await axios.get(`/api/jobs/${jobId}`);
     state.job = response.data;
   } catch (error) {
     console.error(error);
@@ -27,6 +28,7 @@ onMounted(async () => {
 </script>
 
 <template>
+  <BackButton />
   <section v-if="!state.isLoading" class="bg-green-50">
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
